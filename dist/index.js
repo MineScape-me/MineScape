6e219c50cde718e21c4f29b9c00847d2579ea516
AshleyThew <ashl.e.thew@gmail.com>
2022-06-22 14:47:34 +1200

    Update action

--- dist/index.js
+++ dist/index.js
@@ -8994,7 +8994,7 @@ const getAllFiles = function (dirPath, arrayOfFiles) {
 		if (fs.statSync(dirPath + "/" + file).isDirectory()) {
 			arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
 		} else {
-			arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));
+			arrayOfFiles.push(path.join(dirPath, "/", file));
 		}
 	});
 
