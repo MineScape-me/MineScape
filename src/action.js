45e2ac1b306cb1783c6ec1d0f2712e77d12c57a8
AshleyThew <ashl.e.thew@gmail.com>
2022-08-21 08:53:35 +1200

    Fix action

--- src/action.js
+++ src/action.js
@@ -305,7 +305,7 @@ async function run() {
 				Object.entries(trees).forEach(([k, nodes]) => {
 					console.log(typeof nodes);
 					var tree = file + ":" + k + `(${nodes.length})`;
-					nodes.forEach(node =>{
+					Obeject.values(nodes).forEach(node =>{
 						if(nodeIds.has(node.id)){
 							state.issues += `\n\n> **${tree}** duplicate node id with another tree.\n}`;
 						}
